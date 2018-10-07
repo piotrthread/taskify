449ccd6eaccff9cde312c58cf1108f9610e26187
Piotr Niciejewski <piotrthread@gmail.com>
2018-10-07 20:35:19 +0200

    Initial Commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from "./App.scss";
+import { CSSTransitionGroup } from 'react-transition-group';
+
+class App extends React.Component{
+    render(){
+        return <React.Fragment>
+                
+        </React.Fragment>;
+    }
+}
+
+export default App;
